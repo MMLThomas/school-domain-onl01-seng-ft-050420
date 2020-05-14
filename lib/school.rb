c4e6ca8deac0ae7028e8847ac70fdce968e9802d
michael thomas <michael.thomas2014@gmail.com>
2020-05-14 09:54:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,18 +5,15 @@ class School
   
   def initialize(name)
     @name = name
-    roster = {}
+    @roster = {}
   end
   
-  def roster
-    roster = {}
-  end
   
   def add_student(names, grade)
-    if (roster.keys.include?(grade) == false)
-      roster[grade] = [ ]
+    if (@roster.keys.include?(grade) == false)
+      @roster[grade] = [ ]
     end
-    roster.merge!(grade => names)
+    @roster.merge!(grade => names)
     
   end
   
