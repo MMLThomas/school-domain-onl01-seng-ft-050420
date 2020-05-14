7150e0dc17c95673c30063eb2f8e32d6f9288a5d
michael thomas <michael.thomas2014@gmail.com>
2020-05-14 07:13:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,14 @@
 # code here!
 class School
   attr_reader :name
+  roster = {}
   
   def initialize(name)
     @name = name
   end
-
+  
+  def add_student(name, grade)
+    roster[grade] += name
+  end
 
 end
